1f269a515532eeca04b9582714e8ec366481c8e2
Deokhyun Ahn <dks0554@gmail.com>
2026-07-06 16:58:48 +0900

    [test] Add assembler basic test code
    Signed-off-by: Deokhyun Ahn <dks0554@gmail.com>

---
+++ CodeFab/assembler_interface.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "syntax_tree.h"
+
+struct AssemblerInterface {
+    virtual std::unique_ptr<SyntaxTree> assemble(const std::vector<Token> tokens) = 0;
+};
