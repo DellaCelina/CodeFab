166ce99ddd4c65cec85cca9e307b0230029d3193
minji-choe-0559 <mia827.oh@emcore.kr>
2026-07-07 10:53:29 +0900

    [test] AddExpression 테스트 추가
    1 + 2 = 3 을 검증하는 테스트
    Executor에 AddExpression 핸들러가 없어 evaluate()가
    std::logic_error를 던지고 실패한다 (red).

--- CodeFab/ExecutorTest.cpp
+++ CodeFab/ExecutorTest.cpp
@@ -13,3 +13,13 @@ TEST_F(ExecutorTester, Evaluate_NumberLiteral_ReturnsItsValue) {
 
     EXPECT_EQ(result.asNumber(), 1.0);
 }
+
+TEST_F(ExecutorTester, Evaluate_AddExpression_ReturnsSum) {
+    NumberExpression one(1);
+    NumberExpression two(2);
+    AddExpression add(&one, &two);
+
+    Value result = executor.evaluate(&add);
+
+    EXPECT_EQ(result.asNumber(), 3.0);
+}
